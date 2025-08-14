a04cc8d974070961f297d8ccb0a8433a2aae42e9
Patoliyabhoomi <165104541+Patoliyabhoomi@users.noreply.github.com>
2025-08-14 17:03:24 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+# app.py
 from flask import Flask
+
+# This is the Flask application object
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello s12262044"  
+    return "Hello 12262044!"  # Replace with your student ID
 
+# This allows local testing
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8000)
